3e1bee59354afe5d89cd37637b4fa6661b8b5afb
TheFlippantFox <theflippantfox@gmail.com>
2025-02-14 15:05:39 +0000

    feat: added varcel support

--- svelte.config.js
+++ svelte.config.js
@@ -15,14 +15,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
+		adapter: adapter(),
 		prerender: {
 			entries: ['*']
 		}
